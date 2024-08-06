a0aa347604fd23076cc4f7281825f99f1d915224
Allen Jude <ajallen142001@gmail.com>
2024-08-07 00:28:24 +0530

    2nd commit

---
+++ post_fix.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+
+int t=-1;
+int s[MAX];
+
+
+void infixToPostfix(char infix[], char postfix[]){
+
+}
+
+void push(char symbol){
+
+}
+
+char pop(){
+
+}
+
+int isEmpty(){
+
+}
+
+int precedence(char symbol){
+
+}
+
+void main(){
+    
+}
